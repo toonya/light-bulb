e246f2368876a30cba8e9f9b3d574323a3790936
toonya <msc5762@me.com>
2014-05-17 18:36:09 +0800

    homepage beta 1.0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+(function($){
+	// ----------------------------------------
+	// ! add hover support
+	// ----------------------------------------
 
+	$('.tab-nav-hover').on('mouseenter', 'a', function(){
+		//$(this).closest('li').siblings().find('a');
+		$(this).tab('show');
+	})
+
+	$('section.service a img').hover(
+		function(){
+			TweenMax.to(this, .35, {rotation: 10, scale:1.15})
+		},
+		function(){
+			TweenMax.to(this, .35, {rotation: 0, scale:1})
+		}
+	)
+
+	$('.answer-people-list').on('show.bs.tab', 'a', function(e){
+		var $target  = $($(e.target).attr('href'));
+
+		TweenMax.set($target, {opacity:0});
+		TweenMax.to($target, .65, {opacity: 1, delay: .2});
+	})
+
+})(jQuery)
